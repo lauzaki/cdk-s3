c1a6d886fc5ed2a9edbdecbcb93e28da422551ca
Lauzaki <L@L.local>
2021-10-09 16:44:56 +0100

    get presigneed working

--- lambda/get-S3-presignedURL.js
+++ lambda/get-S3-presignedURL.js
@@ -4,12 +4,17 @@ var s3 = new AWS.S3({
 });
 
 
-exports.handler = (event, context, callback) => {
+
+exports.handler = async function(event) {
   const url = s3.getSignedUrl('putObject', {
     Bucket: 'cdkworkshopstack-bucket83908e77-1ojw9gyxpwhp2',
     Key: 'UploadedFile',
     Expires: 600,
   });
 
-  callback(null, url);
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "text/plain" },
+    body: url
+  };
 };
--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -10,13 +10,19 @@ export class CdkWorkshopStack extends cdk.Stack {
     const henLambda = new lambda.Function(this, 'HrMsgHandler', {
       runtime: lambda.Runtime.NODEJS_14_X,    // execution environment
       code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
-      handler: 'hr-on-msg.handler'            // file is "hello", function is "handler"
+      handler: 'hr-on-msg.handler.handler'            // file is "hello", function is "handler"
     });
 
-    const getPressignedUrl = new lambda.Function(this, 'HrGetS3UrlHandler', {
+    const getPresignedUrl = new lambda.Function(this, 'HrGetS3UrlHandler', {
       runtime: lambda.Runtime.NODEJS_14_X,    // execution environment
       code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
-      handler: 'get-S3-presignedURL'            // file is "hello", function is "handler"
+      handler: 'get-S3-presignedURL.handler'            // file is "hello", function is "handler"
+    });
+
+    const hello = new lambda.Function(this, 'hello', {
+      runtime: lambda.Runtime.NODEJS_14_X,    // execution environment
+      code: lambda.Code.fromAsset('lambda'),  // code loaded from "lambda" directory
+      handler: 'hello.handler'            // file is "hello", function is "handler"
     });
 
     // defines an API Gateway REST API resource backed by our "hello" function.
@@ -25,7 +31,12 @@ export class CdkWorkshopStack extends cdk.Stack {
     });
 
     new apigw.LambdaRestApi(this, 'getPressignedUrl', {
-      handler: getPressignedUrl
+      handler: getPresignedUrl
+    });
+
+    // defines an API Gateway REST API resource backed by our "hello" function.
+    new apigw.LambdaRestApi(this, 'MyEndpoint', {
+       handler: hello
     });
 
     const bucket = new s3.Bucket(this, 'HenRadioCompressBucket')
